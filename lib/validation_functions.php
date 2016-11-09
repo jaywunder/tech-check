2771a6b84cdf8204ab87f61fa917f4957428306b
jaywunder <jaywunder@gmail.com>
2016-11-09 12:34:35 -0500

    removed print statements

--- lib/validation_functions.php
+++ lib/validation_functions.php
@@ -13,11 +13,8 @@ function verifyNumeric ($testString) {
 }
 
 function verifyPhone ($testString) {
-	print 'testing '. $testString . '<br>';
 	// Check for usa phone number http://www.php.net/manual/en/function.preg-match.php
 	$regex = '/^(?:1(?:[. -])?)?(?:\((?=\d{3}\)))?([2-9]\d{2})(?:(?<=\(\d{3})\))? ?(?:(?<=\d{3})[.-])?([2-9]\d{2})[. -]?(\d{4})(?: (?i:ext)\.? ?(\d{1,5}))?$/';
-	print 'regex '. $regex . '<br>';
-	print 'match '. preg_match($regex, $testString) . '<br>';
 	return preg_match($regex, $testString);
 }
 
