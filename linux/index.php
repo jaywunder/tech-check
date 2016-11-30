62edfb314206ed9428d64c1d6d6faf4ab55a3792
hurstycuz <thurst333@gmail.com>
2016-11-29 19:17:03 -0500

    create index.php, 45 minutes

---
+++ linux/index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html>
+<head>
+<meta charset='UTF-8'><meta name='viewport' content='width=device-width initial-scale=1'>
+<title>about_linux</title></head>
+<body><h1>About Linux</h1>
+<p></p>
+<p></p>
+<h2>Introduction</h2>
+<p>​	Linux, an operating system originally created by Linus Torvalds, is freely distributed under the terms of the <a href='https://kb.iu.edu/d/acuo'>GNU</a> General Public License. It behaves like Unix, but has a different source code base. Linux offers standard Unix-like features, such as multiuser support, multitasking, and networking. It supports all the <a href='http://pubs.opengroup.org/onlinepubs/000095399/idx/utilities.html'>standard Unix utilities</a> and can compile most major Unix packages with little effort.The complete Linux operating system consists of the Linux core, or kernel, combined with the utilities and applications required for a fully functional operating system. Several versions, called distributions, have been developed commercially and in the open source community. Unlike Unix, which is a registered trademark of The Open Group, Linux has no official version.</p>
+<p></p>
+<h2>Distributions</h2>
+<p>​	Numerous Linux distributions are available, each containing the Linux kernel, a variety of installation and application programs, and other customized features.Some of the earliest distributions, such as Debian, Red Hat, and Slackware, have spawned the development of multiple derivative distributions; for example:</p>
+<ul><li>
+Debian, an open source Linux distribution released in 1993, was used as the basis for developing the Knoppix and Ubuntu distributions. Mint OS is an operating system based off of Ubuntu.</li>
+</ul>
+<ul><li>
+Red Hat Linux, now discontinued, was used as the basis for designing Red Hat Enterprise Linux (RHEL) and its open source counterpart, Fedora. CentOS is a Linux distribution based on RHEL.</li>
+</ul>
+<ul><li>
+Slackware, an improved version of the discontinued Softlanding Linux System (SLS), is an active open source distribution that was used as the basis for developing SUSE Linux, a commercial Linux distribution that spawned the development of openSUSE, its open source counterpart.</li>
+</ul>
+<p> Each distribution has its own individual strengths and weaknesses. Some distributions are very similar to each other, with only minor differences, while others have significant differences. Also, some distributions are designed for specific types of computers.</p>
+<p></p>
+<h2>Why I Like It</h2>
+<p>​	I first started using Linux because of how lightweight and accessible it is. You can download it for free, burn it onto a USB or CD, and then load it onto any computer. This ease of access was why I began using Linux, but the many benefits of the Linux kernel kept me a frequent user. Between the package managing and ease of use for things like LAMP stacks, Linux is a developers best friend. </p>
+<p>​	</p>
+</body>
+</html>
