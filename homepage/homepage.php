e2acb3b8ab1c6f3d6432c1a56d0f94934a7b1500
hurstycuz <thurst333@gmail.com>
2016-12-04 17:48:57 -0500

    Updated Homepage.php, 35 minutes

--- homepage/homepage.php
+++ homepage/homepage.php
@@ -1,1 +1,30 @@
-A bunch of stuff
+
+
+<h1>Welcome to Tech Check</h1>
+<h2>Your home for computer science knowledge and assistance</h2>
+<p></p>
+<h3>What is tech check?</h3>
+<p>We are a not-for-profit organization that specializes in computer science. Consisting of a diverse team of programmers and hardware technicians, Tech Check is a leading competitor in the Techos-Sphere. </p>
+<h3>What is our Mission Statement?</h3>
+<p>Our sole goal is to inform the average consumer about current computer science trends while keeping them up to date on how to troubleshoot and solve any issues that occur with their current devices. We pride ourselves on being lovers of knowledge and we appreciate how drastically technology has increased the free and easy flow of information. </p>
+<h3>Relevant Information:</h3>
+<ul><li>
+<p>You can contact us via E-mail at: <a href='mailto://jwunder@jwunder.com' target='_blank' >jwunder@jwunder.com</a></p>
+</li>
+<li>
+<p>You may call at: 212-xxx-65xx</p>
+</li>
+<li>
+<p>Our hours of operation are:</p>
+<ul><li>
+Mon-Fri: 8AM - 4PM</li>
+<li>
+Saturday: 10AM - 4PM</li>
+<li>
+Sunday: 11AM - 3PM</li>
+</ul>
+</li>
+</ul>
+<p>We really appreciate our strong customer base and strongly encourage you to sign up for our newsletter so we can keep you up to date on all of our weekly updates. Peace, Love, and Technology.</p>
+<p></p>
+<p>--The Tech Check Team</p>
