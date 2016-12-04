71f5f6b5be9a3356f5059577136999aea0b37ab1
kbarrows <16kbarrows@gmail.com>
2016-12-04 13:43:01 -0500

    changed variables - 45 mins

--- index.php
+++ index.php
@@ -1,74 +1,44 @@
-<?php include 'includes/top.php'; 
+<?php include 'includes/top.php'; ?>
   
-  $thisURL = $domain . $phpSelf;
-  $firstName = "";
-  $lastName = "";
-  $email = "";
-   
-  $firstNameERROR = false;
-  $lastNameERROR = false;
-  $emailERROR = false;
+  <?php
+  print_debug($_POST);
   
-  $errorMsg = array();
-  $dataRecord = array();
+  function getFormErrors($post) {
+    $errors = [];
     
-  if (isset($_POST["btnSubmit"])) {
-      
-    if (!securityCheck($thisURL)) {
-      $msg = "<p>Page could not be displayed. ";
-      $msg .= "Security breach was detected and reported.</p>";
-      die($msg);
-    }
-      
-    $firstName = htmlentities($_POST["txtFirstName"], ENT_QUOTES, 'UTF-8');
-    $dataRecord[] = $firstName;
+    if(empty($post)) return ['submit'];
     
-    $lastName = htmlentities($_POST["txtLastName"], ENT_QUOTES, 'UTF-8');
-    $dataRecord[] = $lastName;
-    
-    $email = filter_var($_POST["txtEmail"], FILTER_SANITIZE_EMAIL);
-    $dataRecord[] = $email;
-      
-    if ($firstName == "") {
-      $errorMsg[] = "Please enter your first name.";
-      $firstNameERROR = true;
-    } elseif (!veryifyAlphaNum($firstName)){
-      $errorMsg[] = "Your first name has invalid characters.";
-      $firstNameERROR = true;
-    }
-    if ($lastName == "") {
-      $errorMsg[] = "Please enter your last name.";
-      $lastNameERROR = true;
-    } elseif (!veryifyAlphaNum($lastName)){
-      $errorMsg[] = "Your last name has invalid characters.";
-      $lastNameERROR = true;
-    }
-    if ($email == "") {
-      $errorMsg[] = "Please enter your email address.";
-      $emailERROR = true;
-    } elseif (!verifyEmail($email)){
-      $errorMsg[] = "Please enter a valid email address.";
-      $emailERROR = true;
+    foreach ($post as $name => $value) {
+      if ($value == '') {
+        $errors[] = $name;
+      }
     }
     
-    if ($errorMsg) {
-      if ($debug)
-        print "<p>Error</p>";
-    }  
-      
-    $fileExt = ".csv";
-    $myFileName = "data/newsletterRegister";
-    $fileName = $myFileName . $fileExt;
-      
-    if ($debug){
-      print "\n\n<p>filename is " . $fileName;       
-    }
-      
-    $file = fopen($fileName, 'a');
-    fputcsv($file, $dataRecord);
-    fclose($file); 
+    if (!verifyAlphaNum($post['txtFirstName'])) $errors[] = 'txtFirstName';
+    if (!verifyAlphaNum($post['txtLastName'])) $errors[] = 'txtLastName';
+    if (!verifyAlphaNum ($post['txtEmail'])) $errors[] = 'txtEmail';
     
+    return $errors;
   }
+  
+  $errors = getFormErrors($_POST);
+  
+  /* if (!empty($errors)); */
+          
+  $beenSubmitted = !in_array('submit', $errors);
+  if ($beenSubmitted AND count($errors) > 0){
+    $errors = array_unique($errors);
+    
+    print '<h3>There were errors in your form</h3>';
+    print '<ol> class="error-list">';
+    
+    foreach ($errors as $i => $error){
+      $camelCase = preg_split('/(?=[A-Z])/', substr($error, 3));
+      print '<li class=\"error-listing\"> $camelCase[1] </li>';
+    }
+    
+    print '</ol>';
+  } 
 ?>
 
 <h1>Home</h1>
@@ -77,21 +47,21 @@
 
 <section id="newsletter"> 
   <article>
-  <p>Subscribe to our newsletter "The Tech-Check Times"</p>
-  <form action="<?php print $phpSelf ?>"
+  <form action="<?php echo $ROOT_DIRECTORY?>"
     id="newsletter"
     method="post">
- 
+      
+  <p>Subscribe to our newsletter "<i>The Tech-Check Times</i>"</p>
   <fieldset class="newsletter">
   <legend>Tech-Check Times</legend>
-  <p id="newsletter">* = Required (make this font smaller/intalics/red)</p>
+  <p id="newsletter"><i>* = Required </i></p>
   <label class="required" for="txtFirstName">*First Name:
   <input
     id="txtFirstName"
     maxlength="50"
     name="txtFirstName"
     onfocus="this.select()"
-    placeholder="Enter your first name..."
+    placeholder="First name"
     tabindex="100"
     type="text"
     value="<?php print $firstName ?>"
@@ -104,7 +74,7 @@
     maxlength="50"
     name="txtLastName"
     onfocus="this.select()"
-    placeholder="Enter your last name..."
+    placeholder="Last name"
     tabindex="100"
     type="text"
     value="<?php print $lastName ?>"
@@ -117,7 +87,7 @@
     maxlength="50"
     name="txtEmail"
     onfocus="this.select()"
-    placeholder="Enter your email address..."
+    placeholder="Email address"
     tabindex="100"
     type="text"
     value="<?php print $email ?>"
@@ -135,9 +105,9 @@
   
   <label for="yearly">Yearly:
   <input id="yearly" type="radio" name="information" value="Yearly">
-  </label>
+  </label>  
   <br/>
-  <input class="registerButton" id="btnSubmit" name="btnSubmit" tabindex="900" type="submit" value="Submit" >
+  <button type="submit" value="submitted" name="submit">Submit</button>
   </fieldset>
   </form>
   </article>
