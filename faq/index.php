be20e3c16f066dcd72b774491a332563ceb22018
hurstycuz <thurst333@gmail.com>
2016-12-05 12:43:52 -0500

    Updated FAQ, 20 minutes

--- faq/index.php
+++ faq/index.php
@@ -4,10 +4,11 @@
   <section class="faq">
     <input class="checkbox" type="checkbox" id="section-1"/>
     <label class="container" for="section-1">
-      <h1 class="header">FAQ Question 1</h1>
+      <h1 class="header">How do I take care of my laptop battery?</h1>
       <div class="content">
         <p>
-          Some content
+          Laptop batteries last longer when you perform shallow discharges, keep them in cool places, and unplug them once they reach 100%.
+          To be honest though, Laptop batteries have a finite life and you can't do much to change that, so dont worry too much.
         </p>
       </div>
     </label>
@@ -18,19 +19,11 @@
   <section class="faq">
     <input class="checkbox" type="checkbox" id="section-2"/>
     <label class="container" for="section-2">
-      <h1 class="header">FAQ uestion 2</h1>
+      <h1 class="header">Should I actually click "eject" on my USB drives?</h1>
       <div class="content">
         <p>
-          Hello there
-        </p>
-        <p>
-          this is
-        </p>
-        <p>
-          some info about
-        </p>
-        <p>
-          things and stuff
+          Yes. USB drives don't always write information to drives immediately, so hitting "eject" makes sure every task is complete before you remove your drive.
+         
         </p>
       </div>
     </label>
