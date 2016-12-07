6197730937f303db6ca7bd9be0301e1804acb2ab
hurstycuz <thurst333@gmail.com>
2016-12-06 22:37:30 -0500

    Updated about/index.php, 30 minutes

--- about/index.php
+++ about/index.php
@@ -2,7 +2,14 @@
 
 <h1>About</h1>
 
-<p>We are a not-for-profit organization that specializes in computer science. Consisting of a diverse team of programmers and hardware technicians, Tech Check is a leading competitor in the Techos-Sphere. </p>
+<p>We are a not-for-profit organization that specializes in computer science. Consisting of a diverse team of programmers and hardware technicians, Tech Check is a leading competitor in the Techos-Sphere.
+We firmly believe that the tech world should be open to all that wish to join it. As the years go by, the world is turned more and more into a digital 
+sphere that is accessbile mainly to those who keep themsleves on the forefront of this technology. It is cumbersome, but it is well worth the potential rewards.
+At Tech Check we realize how important it is to usher everyone into the light of technology, so we will continue to writ eenough words for our web developement projects.
+To everyone out there who doesn't know their place in the world, konw this: you have a home at Tech Check. But only if you know computers. And Tech.
+And like all of that good stuff that you have to study at school when you get a Major in Computer Science. So if you get that Major then for sure you can come
+work for us guys here we're super chill we swear. But yea so keep on teching on and look at the end of this page for more information about us 
+and technology and the world.</p>
 
 <p>Our sole goal is to inform the average consumer about current computer science trends while keeping them up to date on how to troubleshoot and solve any issues that occur with their current devices. We pride ourselves on being lovers of knowledge and we appreciate how drastically technology has increased the free and easy flow of information. </p>
 <h3>Relevant Information:</h3>
@@ -26,5 +33,8 @@ Sunday: 11AM - 3PM</li>
 <p>We really appreciate our strong customer base and strongly encourage you to sign up for our newsletter so we can keep you up to date on all of our weekly updates. Peace, Love, and Technology.</p>
 <p></p>
 <p>--The Tech Check Team</p>
+
+<p> Oh hey there, its you again. Yea here is more info: we totally make profit. lied about the non-profit thing. But Hey its ok We're swell guys
+  we swear. Or at least I do. I dont know about the others. teehee. </p>
 
 <?php include '../includes/bottom.php'; ?>
