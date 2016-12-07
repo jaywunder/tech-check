d1108dc8814b265b5393ae7e11e0837295d9d9c1
hurstycuz <thurst333@gmail.com>
2016-12-06 22:27:07 -0500

    Updated faq/index.php, 1 hour

--- faq/index.php
+++ faq/index.php
@@ -1,37 +1,133 @@
 <?php include '../includes/top.php'; ?>
   <link rel="stylesheet" href="style.css">
 
-  <section class="faq">
+   <section class="faq">
     <input class="checkbox" type="checkbox" id="section-1"/>
     <label class="container" for="section-1">
-      <h1 class="header">FAQ Question 1</h1>
+      <h1 class="header">How do I take care of my laptop battery?</h1>
       <div class="content">
         <p>
-          Some content
+          Laptop batteries last longer when you perform shallow discharges, keep them in cool places, and unplug them once they reach 100%.
+          To be honest though, Laptop batteries have a finite life and you can't do much to change that, so dont worry too much.
         </p>
       </div>
     </label>
   </section>
 
+  <br>
+
   <section class="faq">
     <input class="checkbox" type="checkbox" id="section-2"/>
     <label class="container" for="section-2">
-      <h1 class="header">FAQ uestion 2</h1>
+      <h1 class="header">Should I actually click "eject" on my USB drives?</h1>
       <div class="content">
         <p>
-          Hello there
-        </p>
-        <p>
-          this is
-        </p>
-        <p>
-          some info about
-        </p>
-        <p>
-          things and stuff
+          Yes. USB drives don't always write information to drives immediately, so hitting "eject" makes sure every task is complete before you remove your drive.
+         
         </p>
       </div>
     </label>
   </section>
+
+  <br>
+
+    <section class="faq">
+    <input class="checkbox" type="checkbox" id="section-2"/>
+    <label class="container" for="section-2">
+      <h1 class="header">Are Macs actually Virus-proof?</h1>
+      <div class="content">
+        <p>
+         While they are not tehcnically virus-proof, Macs are more resistant to viruses. I dont even know if they are resistant or PC's are just extremely non-resistant,
+          but either way Macs are safer. That's not to say that you won't get a virus on a Mac, it's still completely possible. Just don't click on anything to shady.
+          
+         
+        </p>
+      </div>
+    </label>
+  </section>
+
+  <br>
+
+   <section class="faq">
+    <input class="checkbox" type="checkbox" id="section-2"/>
+    <label class="container" for="section-2">
+      <h1 class="header">Which Operating System is the best for Gaming?</h1>
+      <div class="content">
+        <p>
+        While Mac does offer some good graphics, the obvious answer is Windows. To run most PC games at full settings you need a custom built PC
+          which can only run Windows or Linux, and Linux is not the platform to game on. 
+         
+        </p>
+      </div>
+    </label>
+  </section>
+
+  <br>
+
+   <section class="faq">
+    <input class="checkbox" type="checkbox" id="section-2"/>
+    <label class="container" for="section-2">
+      <h1 class="header">Is it Illegal to Torrent Music?</h1>
+      <div class="content">
+        <p>
+        It's a safe bet that if you have to ask, you just shouldn't do it in the first place. It's not completely illegal, but you are violating copyright 
+          laws by torrenting and can get in trouble with your internet provider if you aren't careful. We don't condone torrenting at all.
+         
+        </p>
+      </div>
+    </label>
+  </section>
+
+  <br>
+
+ <section class="faq">
+    <input class="checkbox" type="checkbox" id="section-2"/>
+    <label class="container" for="section-2">
+      <h1 class="header">What's the best Operating System to develop on?</h1>
+      <div class="content">
+        <p>
+        Mac is usually the best OS to develop on. It allows you to manage packages like a linux computer but has better software support for creating programs.
+          While Windows is commonly used in the business world, Apple computers are the system of choice for developers. They're sleek and efficient machines.
+          
+         
+        </p>
+      </div>
+    </label>
+  </section>
+
+  <br>
+
+ <section class="faq">
+    <input class="checkbox" type="checkbox" id="section-2"/>
+    <label class="container" for="section-2">
+      <h1 class="header">Whats the best computer?</h1>
+      <div class="content">
+        <p>
+        While this question is heavily debated and up to opinion, we personally think that Mac computers are the best. They offer the most polished and sleek 
+          platform with the most reliable history. While the Microsoft Surface Book is an amazing piece of technology, Apple is still at the head of premium copmuters in the tech industry.
+          
+         
+        </p>
+      </div>
+    </label>
+  </section>
+
+<br>
+
+ <section class="faq">
+    <input class="checkbox" type="checkbox" id="section-2"/>
+    <label class="container" for="section-2">
+      <h1 class="header">What is artificial intelligence?></h1>
+      <div class="content">
+        <p>
+       Google defines it as "the theory and development of computer systems able to perform tasks that normally require human intelligence, such as visual perception, speech recognition, decision-making, and translation between languages."
+       This is pretty accurate. You basically run an algorithm that allows the computer to test data and figure out which data is better, effectively learning. It runs
+          these processes teach the computer what it needs to know. Its pretty cool stuff.
+         
+        </p>
+      </div>
+    </label>
+  </section>
+
 
 <?php include '../includes/bottom.php'; ?>
